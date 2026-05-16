4e8fa8e7d4544d70a5f21c59057454324c828163
Jose Ivanov Ramirez Tirado <ramireztiradojose@gmail.com>
2026-05-15 20:25:41 -0700

    Preliminar version 3

---
+++ Postura_App_Final/Postura_App_Final/Postura_App_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Postura_App_FinalApp.swift
+//  Postura_App_Final
+//
+//  Created by Angel Alexander Ramirez Tirado on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Postura_App_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
